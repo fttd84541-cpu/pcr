819cf7c7f776915f828e1913c9bd49be80abe1be
yxsm_rico <fttd84541@gmail.com>
2026-05-27 17:50:41 +0200

    Add Firebase configuration to firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,8 @@
+window.firebaseConfig = {
+  apiKey: "AIzaSyAzjuuppj_zuqA_FRG7WIug68P7SCpbsUI",
+  authDomain: "praguerppcr.firebaseapp.com",
+  projectId: "praguerppcr",
+  storageBucket: "praguerppcr.appspot.com",
+  messagingSenderId: "111189472977",
+  appId: "1:111189472977:web:b123432642b851490cf7f0"
+};
